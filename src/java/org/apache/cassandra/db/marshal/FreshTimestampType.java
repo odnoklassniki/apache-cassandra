9a4c1d5cdd1bddb7aab753c8ed8847e0773dfc61
oleg <oa@odnoklassniki.ru>
2011-06-29 16:12:37 +0300

    This is what is used to sort PhotoMarks.MarksByOwner CF

---
+++ src/java/org/apache/cassandra/db/marshal/FreshTimestampType.java
@@ -0,0 +1,49 @@
+/*
+ * @(#) FreshTimestampType.java
+ * Created 29.06.2011 by oleg
+ * (C) ONE, SIA
+ */
+package org.apache.cassandra.db.marshal;
+
+import java.sql.Timestamp;
+
+
+/**
+ * This expects to find timestamp micros in high 8 bytes and sequence and uniq sequence in lower 8 bytes
+ * of column name.
+ * 
+ * Order is fresh first
+ * 
+ * @author Oleg Anastasyev<oa@hq.one.lv>
+ *
+ */
+public class FreshTimestampType extends BytesType
+{
+    /* (non-Javadoc)
+     * @see org.apache.cassandra.db.marshal.BytesType#compare(byte[], byte[])
+     */
+    @Override
+    public int compare(byte[] o1, byte[] o2)
+    {
+        return -super.compare(o1, o2);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.apache.cassandra.db.marshal.BytesType#getString(byte[])
+     */
+    @Override
+    public String getString(byte[] bytes)
+    {
+        long time = toLong(bytes, 0, 8);
+        
+        return new Timestamp(time/1000).toString()+time%1000+'-'+toLong(bytes, 8, 8);
+    }
+
+    public static final long toLong(byte[] b,int offset,int size) {
+        long l = 0;
+        for (int i=0; i<size; ++i)
+            l |= ((long)b[offset+i]&0xff)<<((size-i-1)<<3);
+        return l;
+    }
+    
+}
