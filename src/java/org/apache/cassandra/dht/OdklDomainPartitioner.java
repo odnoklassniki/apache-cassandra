9a57bdf11fac21fa65c49097cc3fec4c27e007b1
oleg <oa@odnoklassniki.ru>
2011-08-16 11:55:55 +0300

    Реализовать OdklPartitioner для кассандры чтобы партиционировать по доменам пользователей (OL-15011) [cassandra] Разобраться с AssertionError in MerkeTree (OL-15788)

--- src/java/org/apache/cassandra/dht/OdklDomainPartitioner.java
+++ src/java/org/apache/cassandra/dht/OdklDomainPartitioner.java
@@ -8,21 +8,23 @@ package org.apache.cassandra.dht;
 import java.util.Random;
 
 import org.apache.cassandra.db.DecoratedKey;
-import org.apache.cassandra.db.Memtable;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.log4j.Logger;
 
 /**
  * This partitioner expects to find keys as long numbers in form of hex strings, in which last 2 chars represent odnoklassniki domain byte.
- * Say, you have key 168eafd9e, so partitioning will performed by last byte of this string, which is 0x9e.
+ * Say, you have key 168efd9e, so partitioning will performed by last byte of this string, which is 0x9e.
  * 
- * As an internal representation it transforms key to token by placing last 2 chars first in {@link StringToken}.
+ * As an internal representation it transforms key to token by placing last 2 chars first in {@link StringToken}
+ * 
+ * So token of id=168efd9e will be 9e168eafd.
  * 
  * @author Oleg Anastasyev<oa@hq.one.lv>
  *
  */
 public class OdklDomainPartitioner extends OrderPreservingPartitioner
 {
+    
     public static final StringToken MINIMUM = new StringToken("00");
     
     private static final Logger logger = Logger.getLogger(OdklDomainPartitioner.class);
@@ -45,7 +47,7 @@ public class OdklDomainPartitioner extends OrderPreservingPartitioner
     {
         key=key.toLowerCase();
         
-        assert key.length()>0 : "Size of key is minimun single digit to partition odnoklassniki style";
+        assert key.length()>0 : "Size of key is minimum single digit to partition odnoklassniki style";
         
         if (key.length()<2) {
             if (isHexDigit(key.charAt(0)))
@@ -57,17 +59,22 @@ public class OdklDomainPartitioner extends OrderPreservingPartitioner
             return new StringToken(key);
         }
         
-        String token = new StringBuilder(key.length()).append(key.substring(key.length()-2)).append(key.substring(0, key.length()-2)).toString() ;
+        if (key.length()==2 && isHexDigit(key.charAt(0)) && isHexDigit(key.charAt(1)))
+        {
+            return new StringToken(key);
+        }
         
-        assert token.length()==key.length();
+        StringBuilder token = new StringBuilder(key.length()).append(key.substring(key.length()-2));
+        if ( !isHexDigit(token.charAt(0)) || !isHexDigit(token.charAt(1)) )
+        {
+            if (logger.isDebugEnabled())
+                logger.debug("Last 2 chars of key must be hex digits, but in "+key+" they're not. This is ok for system table. reverting to OrderedPartitioner");
+            return new StringToken(key);
+        }
         
-        if ( isHexDigit(token.charAt(0)) && isHexDigit(token.charAt(1)) ) 
-            return new StringToken(token);
+        token.append(key,0,key.length()-2);
         
-        if (logger.isDebugEnabled())
-            logger.debug("Last 2 chars of key must be hex digits, but in "+key+" they're not. This is ok for system table. reverting to OrderedPartitioner");
-        
-        return new StringToken(key);
+        return new StringToken(token.toString());
     }
     
     public DecoratedKey<StringToken> decorateKey(String key)
@@ -78,17 +85,6 @@ public class OdklDomainPartitioner extends OrderPreservingPartitioner
     public DecoratedKey<StringToken> convertFromDiskFormat(String key)
     {
         return new DecoratedKey<StringToken>(toStringToken(key), key);
-    }
-    
-    /* (non-Javadoc)
-     * @see org.apache.cassandra.dht.OrderPreservingPartitioner#midpoint(org.apache.cassandra.dht.StringToken, org.apache.cassandra.dht.StringToken)
-     */
-    @Override
-    public StringToken midpoint(StringToken ltoken, StringToken rtoken)
-    {
-        StringToken midpoint = super.midpoint(ltoken, rtoken);
-        
-        return new StringToken(midpoint.token.substring(0, 2));
     }
     
     /* (non-Javadoc)
